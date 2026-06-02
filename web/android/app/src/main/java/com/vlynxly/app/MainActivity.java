f65562b32f597d86f833d3f559ee9ea1d8d21dc7
shubh-source <shubhkatiyar6513@gmail.com>
2026-06-02 14:37:24 +0530

    feat: setup WebAppInterface for incoming background calls

--- web/android/app/src/main/java/com/vlynxly/app/MainActivity.java
+++ web/android/app/src/main/java/com/vlynxly/app/MainActivity.java
@@ -3,6 +3,11 @@ package com.vlynxly.app;
 import com.getcapacitor.BridgeActivity;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.webkit.JavascriptInterface;
 
 public class MainActivity extends BridgeActivity {
     @Override
@@ -10,5 +15,28 @@ public class MainActivity extends BridgeActivity {
         super.onCreate(savedInstanceState);
         // Bind the interface so JS can call `Android.showIncomingCall(...)`
         this.bridge.getWebView().addJavascriptInterface(new WebAppInterface(this), "Android");
+    }
+}
+
+class WebAppInterface {
+    Context mContext;
+
+    public WebAppInterface(Context c) {
+        mContext = c;
+    }
+
+    @JavascriptInterface
+    public void showIncomingCall(String callerName, String callType) {
+        if (Settings.canDrawOverlays(mContext)) {
+            Intent intent = new Intent(mContext, IncomingCallActivity.class);
+            intent.putExtra("CALLER_NAME", callerName);
+            intent.putExtra("CALL_TYPE", callType);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            mContext.startActivity(intent);
+        } else {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + mContext.getPackageName()));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mContext.startActivity(intent);
+        }
     }
 }
